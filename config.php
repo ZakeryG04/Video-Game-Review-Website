9457fb9f1e91281581ee3ff758af5c46e7fd09fa
Zakery Grosvenor <grosvenorzakery@gmail.com>
2026-03-06 00:36:17 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*Name: Zakery Grosvenor
+Date: December 12th, 2025
+CSC 390
+Project 4
+*/
+
+return [
+    'db_host' => 'localhost',        
+    'db_name' => 'name',
+    'db_user' => 'username',
+    'db_pass' => 'password',
+];
